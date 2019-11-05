acef33da0c722af44df5f3d99bce648a77829cce
bettermetalzsnake <marcmartinezemail@gmail.com>
2019-11-04 17:34:56 -0700

    fixed index.test.js

--- test/server/mixins/utils/index.test.js
+++ test/server/mixins/utils/index.test.js
@@ -5,7 +5,17 @@ const utils = require('../../../../server/mixins/utils')
 
 describe('Utility Functions', () => {
   describe('#updateTimestamps()', () => {
-    it('should update `updatedAt` on an request', (done) => {
+    it('should update `updatedAt` on an instance', (done) => {
+      const context = {
+        instance: {},
+      }
+      utils.updateTimestamps(context, () => {
+        expect(context.instance.updatedAt).to.be.an.instanceof(Date)
+        done()
+      })
+    })
+    // NEW - ADD THIS TEST
+    it('should update `updatedAt` on a request', (done) => {
       const context = {
         data: {},
       }
@@ -14,6 +24,5 @@ describe('Utility Functions', () => {
         done()
       })
     })
-    
   })
 })
